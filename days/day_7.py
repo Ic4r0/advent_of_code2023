2a3c503c0e56815d72dd3552edff592a48e9752c
Ic4r0 <boscaronicola92@gmail.com>
2023-12-09 11:40:29 +0100

    Add day 8 and 9 solutions

--- days/day_7.py
+++ days/day_7.py
@@ -1,4 +1,4 @@
-""" Day 7: NA
+""" Day 7: Camel Cards
 
 Author: Ic4r0 - https://github.com/Ic4r0
 
@@ -6,27 +6,83 @@ Created: 27th November 2023
 """
 
 # imports
-from utils.parse_input import parse_single_line
+from utils.parse_input import parse_by_line
+from collections import Counter
+
+CARDS = {'A': 'E', 'K': 'D', 'Q': 'C', 'J': 'B', 'T': 'A', '9': '9', '8': '8', '7': '7', '6': '6', '5': '5', '4': '4',
+         '3': '3', '2': '2'}
 
 
 # modules
+def mapping_from_cards_to_alphabet(line: tuple, j_as_jokers: bool = False) -> tuple:
+    """ Mapping from given line to alphabetic line to help sorted()
+
+    :param j_as_jokers: boolean parameter for specific mapping for J
+    :param line: line from input list
+    :return: remapped line
+    """
+    cards, bid = line
+    str_list = list(cards)
+    remapped_line = []
+    for ch in str_list:
+        if ch == 'J' and j_as_jokers:
+            remapped_line.append('1')
+        else:
+            remapped_line.append(CARDS[ch])
+    return ''.join(remapped_line), bid
+
+
 def part_1(input_list: list) -> int:
     """ Code for the 1st part of the 7th day of Advent of Code
 
     :param input_list: input list
     :return: numeric result
     """
-    min_value = min(input_list)
-    max_value = max(input_list)
-
-    positions_dict = {
-        value: sum(
-            abs(input_elem - value)
-            for input_elem in input_list
-        ) for value in range(min_value, max_value + 1)
+    results = {
+        # result: [(cards, bid), ...]
+        'Five of a kind': [],
+        'Four of a kind': [],
+        'Full house': [],
+        'Three of a kind': [],
+        'Two pair': [],
+        'One pair': [],
+        'High card': [],
     }
-
-    return min(positions_dict.values())
+    remapped_input_list = [mapping_from_cards_to_alphabet(line) for line in input_list]
+    for hand in remapped_input_list:
+        cards, _ = hand
+        count = dict(Counter(cards))
+        if 5 in count.values():
+            current_point = 'Five of a kind'
+        elif 4 in count.values():
+            current_point = 'Four of a kind'
+        elif 3 in count.values() and 2 in count.values():
+            current_point = 'Full house'
+        elif 3 in count.values():
+            current_point = 'Three of a kind'
+        elif 2 in count.values():
+            number_of_pairs = list(filter(lambda x: (x == 2), count.values()))
+            if len(number_of_pairs) == 2:
+                current_point = 'Two pair'
+            else:
+                current_point = 'One pair'
+        else:
+            current_point = 'High card'
+        results[current_point].append(hand)
+        results[current_point] = sorted(results[current_point], key=lambda x: (x[0]))
+    all_hands = []
+    all_hands.extend(results['High card'])
+    all_hands.extend(results['One pair'])
+    all_hands.extend(results['Two pair'])
+    all_hands.extend(results['Three of a kind'])
+    all_hands.extend(results['Full house'])
+    all_hands.extend(results['Four of a kind'])
+    all_hands.extend(results['Five of a kind'])
+    final_sum = 0
+    for rank, hand in enumerate(all_hands):
+        _, bid = hand
+        final_sum += (rank + 1) * bid
+    return final_sum
 
 
 def part_2(input_list: list) -> int:
@@ -35,17 +91,72 @@ def part_2(input_list: list) -> int:
     :param input_list: input list
     :return: numeric result
     """
-    min_value = min(input_list)
-    max_value = max(input_list)
-
-    positions_dict = {
-        value: sum(
-            sum(range(1, abs(input_elem - value) + 1))
-            for input_elem in input_list
-        ) for value in range(min_value, max_value + 1)
+    results = {
+        # result: [(cards, bid), ...]
+        'Five of a kind': [],
+        'Four of a kind': [],
+        'Full house': [],
+        'Three of a kind': [],
+        'Two pair': [],
+        'One pair': [],
+        'High card': [],
     }
-
-    return min(positions_dict.values())
+    remapped_input_list = [mapping_from_cards_to_alphabet(line, True) for line in input_list]
+    for hand in remapped_input_list:
+        cards, _ = hand
+        count = dict(Counter(cards))
+        j_count = count.get('1', 0)
+        if 5 in count.values():
+            current_point = 'Five of a kind'
+        elif 4 in count.values():
+            if j_count > 0:
+                current_point = 'Five of a kind'
+            else:
+                current_point = 'Four of a kind'
+        elif 3 in count.values() and 2 in count.values():
+            if j_count > 0:
+                current_point = 'Five of a kind'
+            else:
+                current_point = 'Full house'
+        elif 3 in count.values():
+            if j_count > 1:
+                current_point = 'Four of a kind'
+            else:
+                current_point = 'Three of a kind'
+        elif 2 in count.values():
+            number_of_pairs = list(filter(lambda x: (x == 2), count.values()))
+            if len(number_of_pairs) == 2:
+                if j_count == 1:
+                    current_point = 'Full house'
+                elif j_count == 2:
+                    current_point = 'Four of a kind'
+                else:
+                    current_point = 'Two pair'
+            else:
+                if j_count > 0:
+                    current_point = 'Three of a kind'
+                else:
+                    current_point = 'One pair'
+        else:
+            if j_count == 1:
+                current_point = 'One pair'
+            else:
+                current_point = 'High card'
+        results[current_point].append(hand)
+        results[current_point] = sorted(results[current_point], key=lambda x: (x[0]))
+    all_hands = []
+    all_hands.extend(results['High card'])
+    all_hands.extend(results['One pair'])
+    all_hands.extend(results['Two pair'])
+    all_hands.extend(results['Three of a kind'])
+    all_hands.extend(results['Full house'])
+    all_hands.extend(results['Four of a kind'])
+    all_hands.extend(results['Five of a kind'])
+    final_sum = 0
+    for rank, hand in enumerate(all_hands):
+        _, bid = hand
+        final_sum += (rank + 1) * bid
+    return final_sum
 
 
 def day_7(selected_part: int = None, test: bool = False):
@@ -54,7 +165,9 @@ def day_7(selected_part: int = None, test: bool = False):
     :param selected_part: selected Advent of Code part of the 7th day
     :param test: flag to use test input
     """
-    input_list = [int(elem) for elem in parse_single_line(7, is_test=test).split(',')]
+    input_list = parse_by_line(7, int_list=False, is_test=test)
+    splitted = [line.split() for line in input_list]
+    input_list = [(cards, int(bid)) for cards, bid in splitted]
 
     if selected_part == 1 or not selected_part:
         result_part_1 = part_1(input_list[:])
--- days/day_9.py
+++ days/day_9.py
@@ -1,4 +1,4 @@
-""" Day 9: NA
+""" Day 9: Mirage Maintenance
 
 Author: Ic4r0 - https://github.com/Ic4r0
 
@@ -10,56 +10,37 @@ from utils.parse_input import parse_by_line
 
 
 # modules
-def get_low_points(input_list: list) -> list:
-    """ Get list of low points
-
-    :param input_list: input list
-    :return: list of low points
-    """
-    low_points = []
-    for y in range(len(input_list)):
-        for x in range(len(input_list[y])):
-            current_coords = (y, x)     # (row, column)
-            current_value = input_list[y][x]
-
-            north_value = input_list[y-1][x] if y-1 in range(len(input_list)) else 10
-            south_value = input_list[y+1][x] if y+1 in range(len(input_list)) else 10
-            west_value = input_list[y][x-1] if x-1 in range(len(input_list[y])) else 10
-            east_value = input_list[y][x+1] if x+1 in range(len(input_list[y])) else 10
-
-            if current_value < min(north_value, south_value, west_value, east_value):
-                low_points.append([current_coords, current_value])
-
-    return low_points
-
-
-def compute_basin_size(input_list: list, current_coords: tuple) -> int:
-    """ Compute basin size
-
-    :param input_list: input list
-    :param current_coords: current coordinates
-    :return: size of basin
-    """
-    if current_coords not in input_list:
-        return 0
-    basin_size = 1
-    y, x = current_coords
-    next_coords = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
-    input_list.remove(current_coords)
-    for new_coords in next_coords:
-        basin_size += compute_basin_size(input_list, new_coords)
-
-    return basin_size
-
-
 def part_1(input_list: list) -> int:
     """ Code for the 1st part of the 9th day of Advent of Code
 
     :param input_list: input list
     :return: numeric result
     """
-    low_points = get_low_points(input_list)
-    return sum(value + 1 for _, value in low_points)
+    final_difference_value = []
+    for line in input_list:
+        last_values = []
+        to_be_subtracted = line[:]
+        current_results = []
+        idx_subtr = 0
+        while idx_subtr + 1 < len(to_be_subtracted):
+            minuend = to_be_subtracted[idx_subtr + 1]
+            subtrahend = to_be_subtracted[idx_subtr]
+            current_results.append(minuend - subtrahend)
+            if idx_subtr + 2 < len(to_be_subtracted):
+                idx_subtr += 1
+            elif all(res == 0 for res in current_results):
+                current_res = minuend
+                last_values.reverse()
+                for elem in last_values:
+                    current_res += elem
+                final_difference_value.append(current_res)
+                idx_subtr += 2
+            else:
+                last_values.append(minuend)
+                to_be_subtracted = current_results[:]
+                current_results = []
+                idx_subtr = 0
+    return sum(final_difference_value)
 
 
 def part_2(input_list: list) -> int:
@@ -68,16 +49,31 @@ def part_2(input_list: list) -> int:
     :param input_list: input list
     :return: numeric result
     """
-    low_points = get_low_points(input_list)
-    horizontal_max = len(input_list[0])
-    vertical_max = len(input_list)
-    grid_with_no_9 = [(y, x) for x in range(horizontal_max) for y in range(vertical_max) if input_list[y][x] < 9]
-    basin_sizes = []
-    for coords, _ in low_points:
-        size = compute_basin_size(grid_with_no_9, coords)
-        basin_sizes.append(size)
-    basin_sizes.sort(reverse=True)
-    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+    final_difference_value = []
+    for line in input_list:
+        first_values = [line[0]]
+        to_be_subtracted = line[:]
+        current_results = []
+        idx_subtr = 0
+        while idx_subtr + 1 < len(to_be_subtracted):
+            minuend = to_be_subtracted[idx_subtr + 1]
+            subtrahend = to_be_subtracted[idx_subtr]
+            current_results.append(minuend - subtrahend)
+            if idx_subtr + 2 < len(to_be_subtracted):
+                idx_subtr += 1
+            elif all(res == 0 for res in current_results):
+                current_res = 0
+                first_values.reverse()
+                for elem in first_values:
+                    current_res = elem - current_res
+                final_difference_value.append(current_res)
+                idx_subtr += 2
+            else:
+                first_values.append(current_results[0])
+                to_be_subtracted = current_results[:]
+                current_results = []
+                idx_subtr = 0
+    return sum(final_difference_value)
 
 
 def day_9(selected_part: int = None, test: bool = False):
@@ -86,11 +82,13 @@ def day_9(selected_part: int = None, test: bool = False):
     :param selected_part: selected Advent of Code part of the 9th day
     :param test: flag to use test input
     """
-    input_list = [[int(number) for number in list(line)] for line in parse_by_line(9, int_list=False, is_test=test)]
+    input_list = parse_by_line(9, int_list=False, is_test=test)
+    splitted = [line.split() for line in input_list]
+    remapped = [[int(part) for part in separated_line] for separated_line in splitted]
 
     if selected_part == 1 or not selected_part:
-        result_part_1 = part_1(input_list)
+        result_part_1 = part_1(remapped)
         print('The result of 1st part of the 9th day of AoC is: ' + str(result_part_1))
     if selected_part == 2 or not selected_part:
-        result_part_2 = part_2(input_list)
+        result_part_2 = part_2(remapped)
         print('The result of 2nd part of the 9th day of AoC is: ' + str(result_part_2))
