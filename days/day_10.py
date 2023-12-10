9049b9a0c01104227adbc4f188d23c6b4de80d8b
Ic4r0 <boscaronicola92@gmail.com>
2023-12-10 10:15:07 +0100

    Add day 10 part 1 solution

--- days/day_10.py
+++ days/day_10.py
@@ -1,4 +1,4 @@
-""" Day 10: NA
+""" Day 10: Pipe Maze
 
 Author: Ic4r0 - https://github.com/Ic4r0
 
@@ -9,100 +9,56 @@ Created: 27th November 2023
 from utils.parse_input import parse_by_line
 
 
+NEXT_MOVEMENT_BY_DIRECTION = {
+    'd_|': [(1, 0), 'd'],
+    'u_|': [(-1, 0), 'u'],
+    'l_-': [(0, -1), 'l'],
+    'r_-': [(0, 1), 'r'],
+    'd_L': [(0, 1), 'r'],
+    'l_L': [(-1, 0), 'u'],
+    'd_J': [(0, -1), 'l'],
+    'r_J': [(-1, 0), 'u'],
+    'u_7': [(0, -1), 'l'],
+    'r_7': [(1, 0), 'd'],
+    'u_F': [(0, 1), 'r'],
+    'l_F': [(1, 0), 'd']
+}
+
+DIRECTIONS = [[(0, -1), 'l'], [(-1, 0), 'u'], [(0, 1), 'r'], [(1, 0), 'd']]
+
+
 # modules
-def check_corrupted_line(line: str) -> int:
-    """ Check if line is corrupted
-
-    :param line: line to check
-    :return: syntax error score if corrupted or 0
-    """
-    points = {
-        ')': 3,
-        ']': 57,
-        '}': 1197,
-        '>': 25137
-    }
-    corresponding_chunk = {
-        ')': '(',
-        ']': '[',
-        '}': '{',
-        '>': '<'
-    }
-    open_chunks = []
-    for character in line:
-        if character == '(' or character == '[' or character == '{' or character == '<':
-            open_chunks.append(character)
-        else:
-            last_opened_chunk = open_chunks[-1]
-            if corresponding_chunk[character] == last_opened_chunk:
-                open_chunks.pop()
-            else:
-                return points[character]
-    return 0
-
-
-def complete_line(line: str) -> int:
-    """ Complete line and return corresponding score
-
-    :param line: line to complete
-    :return: completion score
-    """
-    points = {
-        ')': 1,
-        ']': 2,
-        '}': 3,
-        '>': 4
-    }
-    corresponding_chunk = {
-        '(': ')',
-        '[': ']',
-        '{': '}',
-        '<': '>'
-    }
-    open_chunks = []
-    for character in line:
-        if character == '(' or character == '[' or character == '{' or character == '<':
-            open_chunks.append(character)
-        else:
-            open_chunks.pop()
-    completion = []
-    for idx in range(len(open_chunks) - 1, -1, -1):
-        completion.append(corresponding_chunk[open_chunks[idx]])
-    score = 0
-    for character in completion:
-        score *= 5
-        score += points[character]
-    return score
-
-
-def part_1(input_list: list) -> int:
+def part_1(input_dict: dict) -> int:
     """ Code for the 1st part of the 10th day of Advent of Code
 
-    :param input_list: input list
+    :param input_dict: input dict
     :return: numeric result
     """
-    scores = 0
-    for line in input_list:
-        scores += check_corrupted_line(line)
-    return scores
+    starting_point = input_dict.get('S')    # coordinates tuple (line, column)
+    steps = 0
+    for direction_tuple, direction in DIRECTIONS:
+        current_pos = tuple(map(sum, zip(starting_point, direction_tuple)))
+        current_val = input_dict.get(current_pos, 0)
+        steps = 1
+        while current_val != 0 and current_val != 'S' and direction + '_' + current_val in NEXT_MOVEMENT_BY_DIRECTION:
+            next_step, direction = NEXT_MOVEMENT_BY_DIRECTION.get(direction + '_' + current_val)
+            current_pos = tuple(map(sum, zip(current_pos, next_step)))
+            current_val = input_dict.get(current_pos, 0)
+            steps += 1
+        if current_val == 'S':
+            break
+    return steps // 2
 
 
-def part_2(input_list: list) -> int:
+def part_2(input_dict: dict) -> int:
     """ Code for the 2nd part of the 10th day of Advent of Code
 
-    :param input_list: input list
+    :param input_dict: input dict
     :return: numeric result
     """
-    idx_to_remove = []
-    for idx, line in enumerate(input_list):
-        if not check_corrupted_line(line):
-            idx_to_remove.append(idx)
-    incomplete_list = [line for idx, line in enumerate(input_list) if idx in idx_to_remove]
-    scores = []
-    for line in incomplete_list:
-        scores.append(complete_line(line))
-    scores.sort()
-    return scores[len(scores)//2]
+    # Algorithm:
+    # https://gamedev.stackexchange.com/questions/141460/how-can-i-fill-the-interior-of-a-closed-loop-on-a-tile-map
+    return 0
 
 
 def day_10(selected_part: int = None, test: bool = False):
@@ -111,11 +67,20 @@ def day_10(selected_part: int = None, test: bool = False):
     :param selected_part: selected Advent of Code part of the 10th day
     :param test: flag to use test input
     """
-    navigation_subsystem = parse_by_line(10, int_list=False, is_test=test)
+    input_list = parse_by_line(10, int_list=False, is_test=test)
+    input_dict = dict()
+    for idx_line, line in enumerate(input_list):
+        for idx_ch, ch in enumerate(line):
+            ch_coordinates = (idx_line, idx_ch)
+            if ch == 'S':
+                input_dict[ch_coordinates] = ch
+                input_dict[ch] = ch_coordinates
+            elif ch != '.':
+                input_dict[ch_coordinates] = ch
 
     if selected_part == 1 or not selected_part:
-        result_part_1 = part_1(navigation_subsystem)
+        result_part_1 = part_1(input_dict)
         print('The result of 1st part of the 10th day of AoC is: ' + str(result_part_1))
     if selected_part == 2 or not selected_part:
-        result_part_2 = part_2(navigation_subsystem)
+        result_part_2 = part_2(input_dict)
         print('The result of 2nd part of the 10th day of AoC is: ' + str(result_part_2))
